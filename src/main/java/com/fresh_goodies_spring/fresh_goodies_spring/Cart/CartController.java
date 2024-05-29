22e25a7a846dc5ee2d010aca42f34136813fb92a
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-29 15:04:10 +0700

    feat(Cart) add: add cartController

---
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/CartController.java
@@ -0,0 +1,10 @@
+package com.fresh_goodies_spring.fresh_goodies_spring.Cart;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/cart")
+public class CartController {
+
+}
