a5f539b4148e8f48e9a877258f4607ec1e26d369
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-29 15:08:36 +0700

    feat(Cart) add: add cart model

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/CartController.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/CartController.java
@@ -1,10 +1,16 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Cart;
 
+import com.fresh_goodies_spring.fresh_goodies_spring.Cart.Service.CartService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/cart")
 public class CartController {
+    private final CartService cartService;
+
+    public CartController(CartService cartService){
+        this.cartService = cartService;
+    }
 
 }
---
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/Model/Cart.java
@@ -0,0 +1,4 @@
+package com.fresh_goodies_spring.fresh_goodies_spring.Cart.Model;
+
+public class Cart {
+}
