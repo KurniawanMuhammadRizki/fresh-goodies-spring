2b14ce65a16b17e943c7bfceebd370445f247545
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-29 13:44:04 +0700

    feat(Products/Service) add: add ProductService

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Products/Service/ProductService.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Products/Service/ProductService.java
@@ -1,7 +1,53 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Products.Service;
 
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.model.Product;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProductService {
+    List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts(){
+        return products;
+    }
+
+    public Optional<Product> getProduct(long id){
+        return products.stream().filter(product -> product.getId() == id).findFirst();
+    }
+
+    public Product addProduct(Product product){
+        boolean exists = products.stream().anyMatch(p -> p.getId() == product.getId());
+        if(exists){
+            //ini belom
+        }
+        products.add(product);
+        return product;
+    }
+
+    public Product updateProduct(Product product){
+        Product currentProduct = products.stream().filter(p -> p.getId() == product.getId()).findFirst().orElse(null);
+
+        if(currentProduct != null){
+            currentProduct.setName(product.getName());
+            currentProduct.setCategory(product.getCategory());
+            currentProduct.setImageUrl(product.getImageUrl());
+            currentProduct.setPrice(product.getPrice());
+            currentProduct.setWeight(product.getWeight());
+            currentProduct.setMetadata(product.getMetadata());
+        }
+        return currentProduct;
+    }
+
+    public void deteleProduct(Long id){
+        Optional<Product> productOptional = getProduct(id);
+        if (productOptional.isPresent()) {
+            products.remove(productOptional.get());
+        } else {
+           //ini belom
+        }
+    }
 }
