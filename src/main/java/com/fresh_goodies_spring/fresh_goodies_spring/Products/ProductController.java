b529a80eef346c5710190ede42972fe72d8acb74
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-29 13:07:57 +0700

    feat(Products) add: ProductController

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Products/ProductController.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Products/ProductController.java
@@ -1,9 +1,15 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Products;
 
 import com.fresh_goodies_spring.fresh_goodies_spring.Products.Service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.model.Product;
+import com.fresh_goodies_spring.fresh_goodies_spring.responses.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/products")
@@ -15,6 +21,26 @@ public class ProductController {
     }
 
     @GetMapping
-    public
+    public ResponseEntity<Response<List<Product>>> getProducts(){
+        return Response.successfulResponse("All product fetched"); //ini belom
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Response<Optional<Product>>> getProduct(@PathVariable long id){
+        //ini belom
+        return Response.successfulResponse("Product detail found");
+    }
+
+    @PutMapping
+    public ResponseEntity<Response<Product>> updateProduct(@RequestBody Product product){
+        // ini belom
+        return Response.successfulResponse("Update product success");
+    }
+
+    @PostMapping
+    public ResponseEntity<Response<Product>> createProduct(@Validated @RequestBody Product product){
+        //ini belom
+        return Response.successfulResponse(HttpStatus.CREATED.value(), "New product created", product);
+    }
 
 }
