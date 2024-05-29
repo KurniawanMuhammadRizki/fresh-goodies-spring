14bb49d31b70d1353688e5d8c6dd163e81cd87d7
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-29 15:17:42 +0700

    feat(Cart/Model) add: add cartItem

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/Model/CartItem.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Cart/Model/CartItem.java
@@ -1,4 +1,20 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Cart.Model;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
 public class CartItem {
+    private long id;
+    @NotNull
+    @NotBlank
+    private long productId;
+    @NotNull
+    @NotBlank
+    private int quantity;
 }
