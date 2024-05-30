ddeee9a9573a07c9b44aa948da0e80a4a6011d9c
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-30 14:33:25 +0700

    feat(Favorite/Service) add: add getFavoriteProducts

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/Impl/FavoriteServiceImpl.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/Impl/FavoriteServiceImpl.java
@@ -3,10 +3,13 @@ package com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.Impl;
 import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Model.Favorite;
 import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.FavoriteService;
 import com.fresh_goodies_spring.fresh_goodies_spring.Products.Service.ProductService;
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.model.Product;
+import com.fresh_goodies_spring.fresh_goodies_spring.exceptions.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FavoriteServiceImpl implements FavoriteService{
@@ -21,6 +24,13 @@ public class FavoriteServiceImpl implements FavoriteService{
     @Override
     public List<Favorite> getFavorites(){
         return favorites;
+    }
+
+    public List<Product> getFavoriteProducts() {
+        return favorites.stream()
+                .map(favorite -> productService.getProduct(favorite.getProductId())
+                        .orElseThrow(() -> new DataNotFoundException("Product with ID " + favorite.getProductId() + " not found.")))
+                .collect(Collectors.toList());
     }
 
 }
