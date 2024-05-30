6cab351a1e5c8704dcf78ff7a45bdd46bfeb92ee
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-30 14:11:24 +0700

    feat(Favorite) add: add favoriteController getFavorite

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/FavoriteController.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/FavoriteController.java
@@ -1,10 +1,33 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Favorite;
 
+import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Model.Favorite;
+import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.FavoriteService;
+import com.fresh_goodies_spring.fresh_goodies_spring.responses.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/favorites")
 public class FavoriteController {
+
+    private final FavoriteService favoriteService;
+
+    public FavoriteController(FavoriteService favoriteService){
+        this.favoriteService = favoriteService;
+    }
+
+    @GetMapping
+    public ResponseEntity<Response<List<Favorite>>> getFavorites(){
+        List<Favorite> favoriteList = favoriteService.getFavorites();
+
+        if(favoriteList.isEmpty()){
+            return Response.successfulResponse("Favorite Empty");
+        }
+        return Response.successfulResponse("All favorites fetched", favoriteList);
+    }
 
 }
