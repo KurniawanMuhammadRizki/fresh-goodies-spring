439f1b3c271d883da08648ec1a727751d486d3ac
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-30 14:29:04 +0700

    feat(Favorite/Service) add: add getFavorites function

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/Impl/FavoriteServiceImpl.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/Impl/FavoriteServiceImpl.java
@@ -1,4 +1,26 @@
 package com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.Impl;
 
-public class FavoriteServiceImpl {
+import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Model.Favorite;
+import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.FavoriteService;
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.Service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FavoriteServiceImpl implements FavoriteService{
+    private final ProductService productService;
+
+    public FavoriteServiceImpl(ProductService productService){
+        this.productService = productService;
+    }
+
+    List<Favorite> favorites = new ArrayList<>();
+
+    @Override
+    public List<Favorite> getFavorites(){
+        return favorites;
+    }
+
 }
