f9f3eff435c1e8abe481d89328559f17d1638167
KurniawanMuhammadRizki <kmr.oblay97@gmail.com>
2024-05-30 14:37:42 +0700

    feat(Favorite) add: add getFavorites function

--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/FavoriteController.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/FavoriteController.java
@@ -2,7 +2,9 @@ package com.fresh_goodies_spring.fresh_goodies_spring.Favorite;
 
 import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Model.Favorite;
 import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service.FavoriteService;
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.model.Product;
 import com.fresh_goodies_spring.fresh_goodies_spring.responses.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,14 +22,22 @@ public class FavoriteController {
         this.favoriteService = favoriteService;
     }
 
+//    @GetMapping
+//    public ResponseEntity<Response<List<Favorite>>> getFavorites(){
+//        List<Favorite> favoriteList = favoriteService.getFavorites();
+//
+//        if(favoriteList.isEmpty()){
+//            return Response.successfulResponse("Favorite Empty");
+//        }
+//        return Response.successfulResponse("All favorites fetched", favoriteList);
+//    }
     @GetMapping
-    public ResponseEntity<Response<List<Favorite>>> getFavorites(){
-        List<Favorite> favoriteList = favoriteService.getFavorites();
-
-        if(favoriteList.isEmpty()){
-            return Response.successfulResponse("Favorite Empty");
+    public ResponseEntity<Response<List<Product>>> getFavorites() {
+        List<Product> favoriteProducts = favoriteService.getFavoriteProducts();
+        if (favoriteProducts.isEmpty()) {
+            return Response.failedResponse(HttpStatus.NOT_FOUND.value(), "No favorite products found");
         }
-        return Response.successfulResponse("All favorites fetched", favoriteList);
+        return Response.successfulResponse("Favorite products fetched successfully", favoriteProducts);
     }
 
 }
--- src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/FavoriteService.java
+++ src/main/java/com/fresh_goodies_spring/fresh_goodies_spring/Favorite/Service/FavoriteService.java
@@ -2,6 +2,7 @@ package com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Service;
 
 import com.fresh_goodies_spring.fresh_goodies_spring.Cart.Model.CartItem;
 import com.fresh_goodies_spring.fresh_goodies_spring.Favorite.Model.Favorite;
+import com.fresh_goodies_spring.fresh_goodies_spring.Products.model.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -9,4 +10,5 @@ import java.util.List;
 public interface FavoriteService {
     List<Favorite> favorites = new ArrayList<>();
     public List<Favorite> getFavorites();
+    public List<Product> getFavoriteProducts();
 }
